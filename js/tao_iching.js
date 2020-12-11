920f190bd1f0a6960b2804bd95d8371e6f7bb23b
ray <ray.lee.burgess@gmail.com>
2020-12-11 17:47:57 -0500

    fix install issue caused by Ajax function call code cleanup

--- js/tao_iching.js
+++ js/tao_iching.js
@@ -63,13 +63,3 @@ jQuery(document).ready(function($)
 		Plotly.newPlot('piechart', data, layout, {responsive: true});
 	}
 });
-
-Backdrop.ajax.prototype.beforeSubmit = function (form_values, element, options)
-{
-  var chk = document.getElementById('tao-iching-reading');
-  if (chk) {
-  coin.innerHTML = '<img class="heads animate-coin" src="/modules/tao_iching/imgs/heads.png"/><img class="heads animate-coin" src="/modules/tao_iching/imgs/heads.png"/><img class="heads animate-coin" src="/modules/tao_iching/imgs/heads.png"/>';
-  }
-};
-
-
--- templates/tao-iching-form.tpl.php
+++ templates/tao-iching-form.tpl.php
@@ -1,3 +1,12 @@
+<script>
+Backdrop.ajax.prototype.beforeSubmit = function (form_values, element, options)
+{
+  var chk = document.getElementById('tao-iching-reading');
+  if (chk) {
+  coin.innerHTML = '<img class="heads animate-coin" src="/modules/tao_iching/imgs/heads.png"/><img class="heads animate-coin" src="/modules/tao_iching/imgs/heads.png"/><img class="heads animate-coin" src="/modules/tao_iching/imgs/heads.png"/>';
+  }
+};
+</script>
 <div class='iching_content'>
 <?php
 $formOutput = backdrop_render_children($form);
